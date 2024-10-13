ceee0eb5ca8ac58ffcb7a0b58f6373fd207327f8
mdmuhafiz <muhafizali02@gmail.com>
2024-10-13 10:23:48 +0530

    2nd commit

---
+++ src/main/java/com/blogapp/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.blogapp.controller;
+
+public class PaymentController {
+    private String paymentType;
+
+    public PaymentController(String paymentType) {
+        this.paymentType = paymentType;
+    }
+}
